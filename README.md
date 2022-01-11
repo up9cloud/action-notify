ba55fc96b6d527c31ec3e335f76bfce7a65bda8d
up9cloud <8325632+up9cloud@users.noreply.github.com>
2022-01-11 15:11:34 -0800

    feat: support drone ci

--- README.md
+++ README.md
@@ -20,6 +20,8 @@ Send notifications all in one.
 
 ## Usage
 
+> GitHub Action
+
 ```yml
 # It won't work on `pull_request`, github won't pass secrets.xxx for the workflow triggered by pull request
 on: [push]
@@ -47,67 +49,62 @@ jobs:
           GITHUB_JOB_STATUS: ${{ needs.deploy.result }}
           TELEGRAM_BOT_TOKEN: ${{secrets.TELEGRAM_BOT_TOKEN}}
           TELEGRAM_CHAT_ID: ${{secrets.TELEGRAM_CHAT_ID}}
-```
-
-## Usage (Custom template)
-
-Template will be parsed by [envsubst](https://www.gnu.org/software/gettext/manual/html_node/envsubst-Invocation.html).
-
-> Template example
-
-```txt
-Run number: ${GITHUB_RUN_NUMBER}
-Commit message: ${GIT_COMMIT_MESSAGE}
-Repo owner: ${CUSTOM_VAR1}
-Custom var: ${CUSTOM_VAR2}
-```
-
-> Workflow example
-
-```yml
-on: [push]
-jobs:
-  deploy:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@v2
-      - name: Notify telegram
-        uses: up9cloud/action-notify@master
-        if: cancelled() == false
-        env:
-          GITHUB_JOB_STATUS: ${{ job.status }}
-          TELEGRAM_BOT_TOKEN: ${{secrets.TELEGRAM_BOT_TOKEN}}
-          TELEGRAM_CHAT_ID: ${{secrets.TELEGRAM_CHAT_ID}}
           # Custom template file path, relative to the repo root
           TELEGRAM_TEMPLATE_PATH: "./test/telegram/custom.txt"
+          # Custom env, assign it from [GitHub context](https://docs.github.com/en/actions/reference/context-and-expression-syntax-for-github-actions#contexts)
           CUSTOM_VAR1: ${{github.repository_owner}}
           CUSTOM_VAR2: "a custom variable"
 ```
 
-## More examples
+See more usage examples: [`.github/workflows/main.yml`](https://github.com/up9cloud/action-notify/blob/master/.github/workflows/main.yml)
 
-See the file: [`.github/workflows/main.yml`](https://github.com/up9cloud/action-notify/blob/master/.github/workflows/main.yml)
+> Drone CI
+
+```yml
+kind: pipeline
+name: after
+
+steps:
+- name: notify
+  image: sstc/action-notify
+  environment:
+    CUSTOM_VAR2: "a custom variable in environment"
+  settings:
+    telegram_bot_token:
+      from_secret: telegram_bot_token
+    telegram_chat_id:
+      from_secret: telegram_chat_id
+    custom_var2: "a custom variable in settings"
+```
+
+## Custom template
+
+Template will be parsed by [envsubst](https://www.gnu.org/software/gettext/manual/html_node/envsubst-Invocation.html), e.q.
+
+```txt
+Commit message: ${GIT_COMMIT_MESSAGE}
+Repo owner: ${CUSTOM_VAR1}
+Custom var: ${CUSTOM_VAR2}
+Custom var in drone plugin settings: ${PLUGIN_CUSTOM_VAR2}
+```
+
+See more template examples: `./template/**/*`
 
 ## Env variables
 
 You can:
 
-- Use Github built-in [default environment variables](https://docs.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables#default-environment-variables)
-- Make custom env variable. Assign from [GitHub context](https://docs.github.com/en/actions/reference/context-and-expression-syntax-for-github-actions#contexts) to env variables or make it by yourself, e.q.
-
-    ```yml
-    env:
-      CUSTOM_VAR1: ${{github.repository_owner}}
-      CUSTOM_VAR2: "a custom var"
-    ```
-
-- Use following built in variables
+- Use CI built-in env
+  - GitHub Action: [default environment variables](https://docs.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables#default-environment-variables)
+  - Drone CI: pipeline [environment reference](https://docs.drone.io/pipeline/environment/reference/) if you're running Drone CI
+- Customize by yourself, see usage
+- Use following built-in env
 
 ### Built-in variables
 
 Common ones:
 
-- `GITHUB_JOB_STATUS`: **Required**, you have to set this to let entrypoint.sh knows job status.
+- `GITHUB_JOB_STATUS`: **Required if** you're using `GitHub Action`, have to set this to let entrypoint.sh knows job status.
 
   ```yml
   env:
@@ -115,7 +112,7 @@ Common ones:
   ```
 
 - `CUSTOM_SCRIPT`: Run custom script, and will **ignore** default action
-  - e.q. if you set this with TELEGRAM_BOT_TOKEN and TELEGRAM_CHAT_ID, it will **only** run your CUSTOM_SCRIPT instead running built-in notify_telegram function
+  - e.q. if you set this with TELEGRAM_BOT_TOKEN and TELEGRAM_CHAT_ID, it will **only** run your CUSTOM_SCRIPT instead trigger built-in notify_telegram function
 - `VERBOSE`: show log or not, true or false
   - *Default value*: false
 
@@ -131,7 +128,7 @@ Platform related:
     - **Required if** want to notify telegram
     - e.q. -123456789
   - `TELEGRAM_PARSE_MODE`: `txt`, `md` or `html`. This will map to telegram [formatting options](https://core.telegram.org/bots/api#formatting-options)
-  - *Default value*: `txt`
+    - *Default value*: `txt`
 - `Slack`
   - `SLACK_TEMPLATE_PATH`: Slack template file path
     - *Default value*: `./template/slack/${TEMPLATE}.json`
@@ -141,7 +138,7 @@ Platform related:
   - `SLACK_API_TOKEN`: Slack api token, various. If you were using bot token, remember `/invite @BOT_NAME` first
     - **Required if** want to notify slack via api
   - `SLACK_CHANNEL`: Slack channel id
-    - **Required if** want to notify slack via api. this also can be used by webhook, but not required
+    - **Required if** want to notify slack via api. (good to have if via webhook)
     - e.q. `#general`
 - `Discord`
   - `DISCORD_TEMPLATE_PATH`: Discord template file path
@@ -156,7 +153,7 @@ Platform related:
   - `LINE_TO`: The user id, group id or chat id. Get it from webhook objects (have to build your server to receive objects)
     - **Required if** want to notify line.me
     - It can be multiple `user` ids, e.q. "Uxxxxxxxxxxx,Uxxxxxxxxxxx", using `,` to separate each id.
-    - Be ware, if you want to notify multiple ids with group or chat id, you `have` to trigger this action one by one.
+    - Be ware, if you want to notify multiple ids mixed with group or chat id, you `have` to trigger this action one by one.
 
       ```yml
       env:
@@ -165,26 +162,41 @@ Platform related:
 
 Template related
 
-- `TEMPLATE`: Built in template style, for now only 2 kinds -- `debug` and `default`, see `./template/<vendor variants>/${TEMPLATE}.<ext>`
-  - *Default value*: default
-- `GITHUB_JOB_STATUS_COLOR`: The RGB color hex code of job status
-- `GITHUB_JOB_SUCCESS_COLOR`: The success color
-  - *Default value*: `#22863a`
-- `GITHUB_JOB_FAILURE_COLOR`: The failure color
-  - *Default value*: `#cb2431`
-- `GITHUB_JOB_CANCELLED_COLOR`: The cancelled color
-  - *Default value*: `#6a737d`
-- `GITHUB_JOB_STATUS_EMOJI`: The emoji of job status.
-- `GITHUB_JOB_SUCCESS_EMOJI`: The success emoji
-  - *Default value*: `🟢`
-- `GITHUB_JOB_FAILURE_EMOJI`: The failure emoji
-  - *Default value*: `🔴`
-- `GITHUB_JOB_CANCELLED_EMOJI`: The cancelled emoji
-  - *Default value*: `⚪️`
-- `GITHUB_SHA_SHORT`: Shorter GITHUB_SHA `cut -c1-8`
-- `GIT_HEAD_COMMIT_MESSAGE`: Event value `.head_commit.message` (See ./test/event.json from GITHUB_EVENT_PATH)
-- `GIT_HEAD_COMMIT_MESSAGE_ESCAPED`: Same as GIT_HEAD_COMMIT_MESSAGE, but escaped, can be safely used in JSON template
-- `GIT_HEAD_COMMITTER_USERNAME`: Event value `.head_commit.committer.username`
-- `GIT_COMMIT_MESSAGE`: Event value `.commits[-1].message`
+- `TEMPLATE`: Built in template style, see `./template/<vendor variants>/${TEMPLATE}.<ext>`
+  - Could be `debug`, `default` or `default.drone`
+  - *Default value*
+    - GitHub Action: default
+    - Drone CI: default.drone
+- `GIT_SHA_SHORT`: Shorter commit sha `cut -c1-8`
+- `GIT_HEAD_COMMIT_MESSAGE`:
+  - GitHub Action: event value `.head_commit.message` (See ./test/event.json from GITHUB_EVENT_PATH)
+  - Drone CI: ""
+- `GIT_HEAD_COMMIT_MESSAGE_ESCAPED`: same as GIT_HEAD_COMMIT_MESSAGE, but escaped, can be safely used in JSON template
+  - e.q. `{"msg":"${GIT_HEAD_COMMIT_MESSAGE_ESCAPED}"}`
+- `GIT_HEAD_COMMITTER_USERNAME`
+  - GitHub Action: event value `.head_commit.committer.username`
+  - Drone CI: ""
+- `GIT_COMMIT_MESSAGE`
+  - GitHub Action: event value `.commits[-1].message`
+  - Drone CI: same as DRONE_COMMIT_MESSAGE
 - `GIT_COMMIT_MESSAGE_ESCAPED`: Same as GIT_COMMIT_MESSAGE, but escaped, can be safely used in JSON template
-- `GIT_COMMITTER_USERNAME`: Event value `.commits[-1].committer.username`
+  - e.q. `{"msg":"${GIT_COMMIT_MESSAGE_ESCAPED}"}`
+- `GIT_COMMITTER_USERNAME`
+  - GitHub Action: event value `.commits[-1].committer.username`
+  - Drone CI: same as DRONE_COMMIT_AUTHOR
+
+- `STATUS_COLOR`: The RGB color hex code of job status
+- `STATUS_COLOR_DECIMAL`: The RGB color decimal code of job status
+- `STATUS_COLOR_SUCCESS`: The success color
+  - *Default value*: `#22863a`
+- `STATUS_COLOR_FAILURE`: The failure color
+  - *Default value*: `#cb2431`
+- `STATUS_COLOR_CANCELLED`: The cancelled color
+  - *Default value*: `#6a737d`
+- `STATUS_EMOJI`: The emoji of job status.
+- `STATUS_EMOJI_SUCCESS`: The success emoji
+  - *Default value*: `🟢`
+- `STATUS_EMOJI_FAILURE`: The failure emoji
+  - *Default value*: `🔴`
+- `STATUS_EMOJI_CANCELLED`: The cancelled emoji
+  - *Default value*: `⚪️`
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,90 +10,176 @@ function die() {
 	exit 1
 }
 
+function setup_keys() {
+	local prefix="$1"
+	K_CUSTOM_SCRIPT="${prefix}CUSTOM_SCRIPT"
+	K_VERBOSE="${prefix}VERBOSE"
+	K_TELEGRAM_TEMPLATE_PATH="${prefix}TELEGRAM_TEMPLATE_PATH"
+	K_TELEGRAM_BOT_TOKEN="${prefix}TELEGRAM_BOT_TOKEN"
+	K_TELEGRAM_CHAT_ID="${prefix}TELEGRAM_CHAT_ID"
+	K_TELEGRAM_PARSE_MODE="${prefix}TELEGRAM_PARSE_MODE"
+	K_SLACK_TEMPLATE_PATH="${prefix}SLACK_TEMPLATE_PATH"
+	K_SLACK_WEBHOOK_URL="${prefix}SLACK_WEBHOOK_URL"
+	K_SLACK_API_TOKEN="${prefix}SLACK_API_TOKEN"
+	K_SLACK_CHANNEL="${prefix}SLACK_CHANNEL"
+	K_DISCORD_TEMPLATE_PATH="${prefix}DISCORD_TEMPLATE_PATH"
+	K_DISCORD_WEBHOOK_URL="${prefix}DISCORD_WEBHOOK_URL"
+	K_LINE_TEMPLATE_PATH="${prefix}LINE_TEMPLATE_PATH"
+	K_LINE_CHANNEL_ACCESS_TOKEN="${prefix}LINE_CHANNEL_ACCESS_TOKEN"
+	K_LINE_TO="${prefix}LINE_TO"
+	K_TEMPLATE="${prefix}TEMPLATE"
+	K_STATUS_COLOR_SUCCESS="${prefix}STATUS_COLOR_SUCCESS"
+	K_STATUS_COLOR_FAILURE="${prefix}STATUS_COLOR_FAILURE"
+	K_STATUS_COLOR_CANCELLED="${prefix}STATUS_COLOR_CANCELLED"
+	K_STATUS_EMOJI_SUCCESS="${prefix}STATUS_EMOJI_SUCCESS"
+	K_STATUS_EMOJI_FAILURE="${prefix}STATUS_EMOJI_FAILURE"
+	K_STATUS_EMOJI_CANCELLED="${prefix}STATUS_EMOJI_CANCELLED"
+}
+
 ################
 # Setup github #
 ################
-if [ -f "$GITHUB_EVENT_PATH" ]; then
+
+# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
+if [ "$GITHUB_ACTIONS" == "true" ]; then
 	export GIT_HEAD_COMMIT_MESSAGE=$(cat "$GITHUB_EVENT_PATH" | jq -cr .head_commit.message || printf '')
 	export GIT_HEAD_COMMIT_MESSAGE_ESCAPED=$(printf "$GIT_HEAD_COMMIT_MESSAGE" | jq -RsM | sed -e 's/^"//' -e 's/"$//')
 	export GIT_HEAD_COMMITTER_USERNAME=$(cat "$GITHUB_EVENT_PATH" | jq -cr .head_commit.committer.username || printf '')
 	export GIT_COMMIT_MESSAGE=$(cat "$GITHUB_EVENT_PATH" | jq -cr .commits[-1].message || printf '')
 	export GIT_COMMIT_MESSAGE_ESCAPED=$(printf "$GIT_COMMIT_MESSAGE" | jq -RsM | sed -e 's/^"//' -e 's/"$//')
 	export GIT_COMMITTER_USERNAME=$(cat "$GITHUB_EVENT_PATH" | jq -cr .commits[-1].committer.username || printf '')
+	export GIT_SHA_SHORT=$(echo "$GITHUB_SHA" | cut -c1-8)
+
+	setup_keys
+
+	# backward compatible
+	export GITHUB_SHA_SHORT="$GIT_SHA_SHORT"
+	if [ -n "$GITHUB_JOB_SUCCESS_COLOR" ]; then
+		STATUS_COLOR_SUCCESS="$GITHUB_JOB_SUCCESS_COLOR"
+	fi
+	if [ -n "$GITHUB_JOB_FAILURE_COLOR" ]; then
+		STATUS_COLOR_FAILURE="$GITHUB_JOB_FAILURE_COLOR"
+	fi
+	if [ -n "$GITHUB_JOB_CANCELLED_COLOR" ]; then
+		STATUS_COLOR_CANCELLED="$GITHUB_JOB_CANCELLED_COLOR"
+	fi
+	if [ -n "$GITHUB_JOB_SUCCESS_EMOJI" ]; then
+		STATUS_EMOJI_SUCCESS="$GITHUB_JOB_SUCCESS_EMOJI"
+	fi
+	if [ -n "$GITHUB_JOB_FAILURE_EMOJI" ]; then
+		STATUS_EMOJI_FAILURE="$GITHUB_JOB_FAILURE_EMOJI"
+	fi
+	if [ -n "$GITHUB_JOB_CANCELLED_EMOJI" ]; then
+		STATUS_EMOJI_CANCELLED="$GITHUB_JOB_CANCELLED_EMOJI"
+	fi
 fi
-if [ -n "$GITHUB_SHA" ]; then
-	export GITHUB_SHA_SHORT=$(echo "$GITHUB_SHA" | cut -c1-8)
+
+############
+# Drone ci #
+############
+
+# https://docs.drone.io/pipeline/environment/reference/
+if [ "$DRONE" == "true" ]; then
+	export GIT_COMMIT_MESSAGE="$DRONE_COMMIT_MESSAGE"
+	export GIT_COMMIT_MESSAGE_ESCAPED=$(printf "$GIT_COMMIT_MESSAGE" | jq -RsM | sed -e 's/^"//' -e 's/"$//')
+	export GIT_COMMITTER_USERNAME="$DRONE_COMMIT_AUTHOR"
+	export GIT_SHA_SHORT=$(echo "$DRONE_COMMIT_SHA" | cut -c1-8)
+
+	setup_keys PLUGIN_
 fi
-function set_job_status_color() {
+
+################
+# Setup status #
+################
+
+function set_status_color() {
+	local status="$1"
 	local color="#ffffff"
-	case "$GITHUB_JOB_STATUS" in
+	case "$status" in
 	success)
-		if [ -n "$GITHUB_JOB_SUCCESS_COLOR" ]; then
-			color="$GITHUB_JOB_SUCCESS_COLOR"
+		if [ -n "${!K_STATUS_COLOR_SUCCESS}" ]; then
+			color="${!K_STATUS_COLOR_SUCCESS}"
 		else
 			color="#22863a"
 		fi
 		;;
 	failure)
-		if [ -n "$GITHUB_JOB_FAILURE_COLOR" ]; then
-			color="$GITHUB_JOB_FAILURE_COLOR"
+		if [ -n "${!K_STATUS_COLOR_FAILURE}" ]; then
+			color="${!K_STATUS_COLOR_FAILURE}"
 		else
 			color="#cb2431"
 		fi
 		;;
 	cancelled)
-		if [ -n "$GITHUB_JOB_CANCELLED_COLOR" ]; then
-			color="$GITHUB_JOB_CANCELLED_COLOR"
+		if [ -n "${!K_STATUS_COLOR_CANCELLED}" ]; then
+			color="${!K_STATUS_COLOR_CANCELLED}"
 		else
 			color="#6a737d"
 		fi
 		;;
 	esac
-	export GITHUB_JOB_STATUS_COLOR="$color"
+	export STATUS_COLOR="$color"
+	export STATUS_COLOR_DECIMAL="$((16${STATUS_COLOR}))"
+
+	# backward compatible
+	export GITHUB_JOB_STATUS_COLOR="$STATUS_COLOR"
+	export GITHUB_JOB_STATUS_COLOR_DECIMAL="$STATUS_COLOR_DECIMAL"
 }
-function set_job_status_emoji() {
+function set_status_emoji() {
+	local status="$1"
 	local emoji="🛎"
-	case "$GITHUB_JOB_STATUS" in
+	case "$status" in
 	success)
-		if [ -n "$GITHUB_JOB_SUCCESS_EMOJI" ]; then
-			emoji="$GITHUB_JOB_SUCCESS_EMOJI"
+		if [ -n "${!K_STATUS_EMOJI_SUCCESS}" ]; then
+			emoji="${!K_STATUS_EMOJI_SUCCESS}"
 		else
 			emoji="🟢"
 		fi
 		;;
 	failure)
-		if [ -n "$GITHUB_JOB_FAILURE_EMOJI" ]; then
-			emoji="$GITHUB_JOB_FAILURE_EMOJI"
+		if [ -n "${!K_STATUS_EMOJI_FAILURE}" ]; then
+			emoji="${!K_STATUS_EMOJI_FAILURE}"
 		else
 			emoji="🔴"
 		fi
 		;;
 	cancelled)
-		if [ -n "$GITHUB_JOB_CANCELLED_EMOJI" ]; then
-			emoji="$GITHUB_JOB_CANCELLED_EMOJI"
+		if [ -n "${!K_STATUS_EMOJI_CANCELLED}" ]; then
+			emoji="${!K_STATUS_EMOJI_CANCELLED}"
 		else
 			emoji="⚪️"
 		fi
 		;;
 	esac
-	export GITHUB_JOB_STATUS_EMOJI="$emoji"
+	export STATUS_EMOJI="$emoji"
+
+	# backward compatible
+	export GITHUB_JOB_STATUS_EMOJI="$STATUS_EMOJI"
 }
-if [ -n "$GITHUB_JOB_STATUS" ]; then
-	set_job_status_color
-	set_job_status_emoji
+if [ "$GITHUB_ACTIONS" == "true" ]; then
+	set_status_color "$GITHUB_JOB_STATUS"
+	set_status_emoji "$GITHUB_JOB_STATUS"
+elif [ "$DRONE" == "true" ]; then
+	set_status_color "$DRONE_BUILD_STATUS"
+	set_status_emoji "$DRONE_BUILD_STATUS"
 fi
 
 ##################
 # Setup template #
 ##################
-if [ -z "$TEMPLATE" ]; then
-	TEMPLATE=default
+
+if [ -z "${!K_TEMPLATE}" ]; then
+	if [ "$GITHUB_ACTIONS" == "true" ]; then
+		TEMPLATE=default
+	elif [ "$DRONE" == "true" ]; then
+		TEMPLATE=default.drone
+	fi
 fi
 function set_default_telegram_template_path() {
 	local ext=txt
-	case "$TELEGRAM_PARSE_MODE" in
+	case "${!K_TELEGRAM_PARSE_MODE}" in
 	html | md)
-		ext=$TELEGRAM_PARSE_MODE
+		ext=${!K_TELEGRAM_PARSE_MODE}
 		;;
 	HTML)
 		ext=html
@@ -104,25 +190,33 @@ function set_default_telegram_template_path() {
 	esac
 	TELEGRAM_TEMPLATE_PATH="/template/telegram/${TEMPLATE}.${ext}"
 }
-if [ -z "$TELEGRAM_TEMPLATE_PATH" ]; then
+if [ -z "${!K_TELEGRAM_TEMPLATE_PATH}" ]; then
 	set_default_telegram_template_path
+else
+	TELEGRAM_TEMPLATE_PATH="${!K_TELEGRAM_TEMPLATE_PATH}"
 fi
-if [ -z "$SLACK_TEMPLATE_PATH" ]; then
+if [ -z "${!K_SLACK_TEMPLATE_PATH}" ]; then
 	SLACK_TEMPLATE_PATH="/template/slack/${TEMPLATE}.json"
+else
+	SLACK_TEMPLATE_PATH="${!K_SLACK_TEMPLATE_PATH}"
 fi
-if [ -z "$DISCORD_TEMPLATE_PATH" ]; then
+if [ -z "${!K_DISCORD_TEMPLATE_PATH}" ]; then
 	DISCORD_TEMPLATE_PATH="/template/discord/${TEMPLATE}.json"
+else
+	DISCORD_TEMPLATE_PATH="${!K_DISCORD_TEMPLATE_PATH}"
 fi
-if [ -z "$LINE_TEMPLATE_PATH" ]; then
+if [ -z "${!K_LINE_TEMPLATE_PATH}" ]; then
 	LINE_TEMPLATE_PATH="/template/line.me/${TEMPLATE}.json"
+else
+	LINE_TEMPLATE_PATH="${!K_LINE_TEMPLATE_PATH}"
 fi
 
 ##########
 # Custom #
 ##########
-if [ -n "$CUSTOM_SCRIPT" ]; then
-	log "\$CUSTOM_SCRIPT detected, skip default notifying."
-	eval "$CUSTOM_SCRIPT"
+if [ -n "${!K_CUSTOM_SCRIPT}" ]; then
+	log "\$$K_CUSTOM_SCRIPT detected, skip default notifying."
+	eval "${!K_CUSTOM_SCRIPT}"
 	exit 0
 fi
 
@@ -133,49 +227,49 @@ __count=0
 
 function default_notify_telegram() {
 	local arg_p=""
-	if [ -n "$TELEGRAM_PARSE_MODE" ]; then
-		arg_p=" -p ${TELEGRAM_PARSE_MODE}"
+	if [ -n "${!K_TELEGRAM_PARSE_MODE}" ]; then
+		arg_p=" -p ${!K_TELEGRAM_PARSE_MODE}"
 	fi
-	local cmd=$(printf 'cat %s | envsubst | tg -q%s' "$TELEGRAM_TEMPLATE_PATH" "$arg_p")
+	local cmd=$(printf 'cat %s | envsubst | tg -q%s' "${!K_TELEGRAM_TEMPLATE_PATH}" "$arg_p")
 	eval "$cmd"
 }
-if [ -n "$TELEGRAM_BOT_TOKEN" ] && [ -n "$TELEGRAM_CHAT_ID" ]; then
-	log "\$TELEGRAM_BOT_TOKEN \$TELEGRAM_CHAT_ID detected, run default telegram notifying."
+if [ -n "${!K_TELEGRAM_BOT_TOKEN}" ] && [ -n "${!K_TELEGRAM_CHAT_ID}" ]; then
+	log "\$$K_TELEGRAM_BOT_TOKEN \$$K_TELEGRAM_CHAT_ID detected, run default telegram notifying."
 	__count=$((__count + 1))
 	default_notify_telegram
 fi
 
 function default_notify_slack_via_webhook() {
 	local parsed_file=$(mktemp)
-	cat "$SLACK_TEMPLATE_PATH" | envsubst | jq --arg channel "${SLACK_CHANNEL}" '. + {channel: $channel}' >$parsed_file
-	local cmd=$(printf 'curl -sSL -X POST -H "content-type: application/json" --data @"%s" "%s"' "$parsed_file" "$SLACK_WEBHOOK_URL")
+	cat "${!K_SLACK_TEMPLATE_PATH}" | envsubst | jq --arg channel "${!K_SLACK_CHANNEL}" '. + {channel: $channel}' >$parsed_file
+	local cmd=$(printf 'curl -sSL -X POST -H "content-type: application/json" --data @"%s" "%s"' "$parsed_file" "${!K_SLACK_WEBHOOK_URL}")
 	eval "$cmd"
 }
 function default_notify_slack_via_api() {
 	local parsed_file=$(mktemp)
 	local url=https://slack.com/api/chat.postMessage
-	cat "$SLACK_TEMPLATE_PATH" | envsubst | jq --arg channel "${SLACK_CHANNEL}" '. + {channel: $channel}' >$parsed_file
-	local cmd=$(printf 'curl -sSL -X POST -H "authorization: Bearer %s" -H "content-type: application/json; charset=UTF-8" --data @"%s" "%s"' "$SLACK_API_TOKEN" "$parsed_file" "$url")
+	cat "${!K_SLACK_TEMPLATE_PATH}" | envsubst | jq --arg channel "${!K_SLACK_CHANNEL}" '. + {channel: $channel}' >$parsed_file
+	local cmd=$(printf 'curl -sSL -X POST -H "authorization: Bearer %s" -H "content-type: application/json; charset=UTF-8" --data @"%s" "%s"' "${!K_SLACK_API_TOKEN}" "$parsed_file" "$url")
 	eval "$cmd"
 }
-if [ -n "$SLACK_WEBHOOK_URL" ]; then
-	log "\$SLACK_WEBHOOK_URL detected, run default slack (webhook) notifying."
+if [ -n "${!K_SLACK_WEBHOOK_URL}" ]; then
+	log "\$$_K_SLACK_WEBHOOK_URL detected, run default slack (webhook) notifying."
 	__count=$((__count + 1))
 	default_notify_slack_via_webhook
-elif [ -n "$SLACK_API_TOKEN" ] && [ -n "$SLACK_CHANNEL" ]; then
-	log "\$SLACK_API_TOKEN \$SLACK_CHANNEL detected, run default slack (api) notifying."
+elif [ -n "${!K_SLACK_API_TOKEN}" ] && [ -n "${!K_SLACK_CHANNEL}" ]; then
+	log "\$$K_SLACK_API_TOKEN \$$K_SLACK_CHANNEL detected, run default slack (api) notifying."
 	__count=$((__count + 1))
 	default_notify_slack_via_api
 fi
 
 function default_notify_discord_via_webhook() {
 	local parsed_file=$(mktemp)
-	cat "$DISCORD_TEMPLATE_PATH" | GITHUB_JOB_STATUS_COLOR="$((16${GITHUB_JOB_STATUS_COLOR}))" envsubst >$parsed_file
-	local cmd=$(printf 'curl -sSL -X POST -H "content-type: application/json" --data @"%s" "%s"' "$parsed_file" "$DISCORD_WEBHOOK_URL")
+	cat "${!K_DISCORD_TEMPLATE_PATH}" | envsubst >$parsed_file
+	local cmd=$(printf 'curl -sSL -X POST -H "content-type: application/json" --data @"%s" "%s"' "$parsed_file" "${!K_DISCORD_WEBHOOK_URL}")
 	eval "$cmd"
 }
-if [ -n "$DISCORD_WEBHOOK_URL" ]; then
-	log "\$DISCORD_WEBHOOK_URL detected, run default discord (webhook) notifying."
+if [ -n "${!K_DISCORD_WEBHOOK_URL}" ]; then
+	log "\$$K_DISCORD_WEBHOOK_URL detected, run default discord (webhook) notifying."
 	__count=$((__count + 1))
 	default_notify_discord_via_webhook
 fi
@@ -183,21 +277,21 @@ fi
 function default_notify_line() {
 	local parsed_file=$(mktemp)
 	local mode=push
-	local LINE_TO_JSON="\"$LINE_TO\""
-	if [[ "$LINE_TO" == *","* ]]; then
-		LINE_TO_JSON=$(echo -en "$LINE_TO" | jq --raw-input --slurp 'split(",")')
+	local LINE_TO_JSON="\"${!K_LINE_TO}\""
+	if [[ "${!K_LINE_TO}" == *","* ]]; then
+		LINE_TO_JSON=$(echo -en "${!K_LINE_TO}" | jq --raw-input --slurp 'split(",")')
 		mode=multicast
 	fi
-	cat "$LINE_TEMPLATE_PATH" | envsubst | jq --argjson to "${LINE_TO_JSON}" '. + {to: $to}' >$parsed_file
-	local cmd=$(printf 'curl -sSL -X POST -H "content-type: application/json" -H "Authorization: Bearer %s" --data @"%s" "https://api.line.me/v2/bot/message/%s"' "$LINE_CHANNEL_ACCESS_TOKEN" "$parsed_file" "$mode")
+	cat "${!K_LINE_TEMPLATE_PATH}" | envsubst | jq --argjson to "${!K_LINE_TO_JSON}" '. + {to: $to}' >$parsed_file
+	local cmd=$(printf 'curl -sSL -X POST -H "content-type: application/json" -H "Authorization: Bearer %s" --data @"%s" "https://api.line.me/v2/bot/message/%s"' "${!K_LINE_CHANNEL_ACCESS_TOKEN}" "$parsed_file" "$mode")
 	exec 3>&1
-	HTTP_STATUS=$(eval "$cmd" -w '%{http_code}' -o >(cat >&3))
-	if ((HTTP_STATUS > 399)); then
-		die "Line api returned http code: ${HTTP_STATUS}"
+	local http_status=$(eval "$cmd" -w '%{http_code}' -o >(cat >&3))
+	if ((http_status > 399)); then
+		die "Line api returned http code: ${http_status}"
 	fi
 }
-if [ -n "$LINE_CHANNEL_ACCESS_TOKEN" ] && [ -n "$LINE_TO" ]; then
-	log "\$LINE_CHANNEL_ACCESS_TOKEN \$LINE_TO detected, run default line notifying."
+if [ -n "${!K_LINE_CHANNEL_ACCESS_TOKEN}" ] && [ -n "${!K_LINE_TO}" ]; then
+	log "\$$K_LINE_CHANNEL_ACCESS_TOKEN \$LINE_TO detected, run default line notifying."
 	__count=$((__count + 1))
 	default_notify_line
 fi
